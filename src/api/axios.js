1d59506429def1950d0343301cb1ed43de7a05bb
JanderC <janderalexisc@gmail.com>
2026-06-29 11:37:55 -0400

    cambios en la url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3001/api'
+  baseURL: process.env.REACT_APP_API_URL || 'https://resourceful-kindness-production-8a18.up.railway.app/api'
 });
 
 API.interceptors.request.use((config) => {
